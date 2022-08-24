1e776a0070b0d4f705412c2ac9ee9740fd9dc88c
JadenFicklin <jadendficklin@gmail.com>
2022-08-24 11:17:23 -0600

    home page styling layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home-outer">
+        <div className="home-left">
+          <div className="home-left-top">Jaden's Shoes</div>
+          <div className="home-left-bot">
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item"></div>
+          </div>
+        </div>
+        <div className="home-right">
+          <div className="home-right-top"></div>
+          <div className="home-right-middle">
+            <div className="home-right-middle-item"></div>
+          </div>
+          <div className="home-right-bottom">
+            <div className="proceed-to-checkout"> Proceed to checkout </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
