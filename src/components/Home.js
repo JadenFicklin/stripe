4b1183592554273d9667abb49c20b6e84a51c631
JadenFicklin <jadendficklin@gmail.com>
2022-08-24 12:45:02 -0600

    styling for shoes, may try to implement a makeshift databse as object holding shoes info

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,60 @@ function Home() {
         <div className="home-left">
           <div className="home-left-top">Jaden's Shoes</div>
           <div className="home-left-bot">
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
-            <div className="home-left-bot-item"></div>
+            <div className="home-left-bot-item one">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item two">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item three">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item four">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item five">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item six">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item seven">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item eight">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
+            <div className="home-left-bot-item nine">
+              <div className="img-top">
+                <div className="img-add-to-cart">+</div>
+              </div>
+              <div className="img-bot"></div>
+            </div>
           </div>
         </div>
         <div className="home-right">
